74cdaa7e61637a26ae3e202e42f72598f77c54c8
felipeschirmann@gmail.com <felipeschirmann@gmail.com>
2022-06-15 02:28:29 -0300

    Navbar created and initial customization

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,28 @@
+import './styles.css';
+const Navbar = () => {
+  return (
+    <>
+      <nav className="bg-primary navbar-expand-md bg-primary main-nav">
+        <div className="container-fluid">
+          <div className="nav-logo-text">
+            <a href="/">
+              <h4>Carros Top</h4>
+            </a>
+          </div>
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav offset-md-3 main-menu">
+              <li>
+                <a href="/" className='active' >HOME</a>
+              </li>
+              <li>
+                <a href="link">CATÁLOGO</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
