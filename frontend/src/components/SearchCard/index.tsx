1dc9c79444a8ab2d70a9d83360a2cf91d6a6120b
felipeschirmann@gmail.com <felipeschirmann@gmail.com>
2022-06-28 05:12:33 -0300

    Create SearchCard

---
+++ frontend/src/components/SearchCard/index.tsx
@@ -0,0 +1,13 @@
+import './styles.css';
+import Button from 'components/Button';
+
+const ProductCard = () => {
+  return (
+    <div className="base-card serch-card">
+      <input type="text" />
+      <Button />
+    </div>
+  );
+};
+
+export default ProductCard;
--- frontend/src/pages/Catalog/index.tsx
+++ frontend/src/pages/Catalog/index.tsx
@@ -1,12 +1,14 @@
 import './styles.css';
 import Navbar from 'components/Navbar';
 import ProductCard from 'components/ProductCard';
+import SearchCard from 'components/SearchCard'
 
 const Catalog = () => {
   return (
     <div>
       <Navbar />
       <div className="container my-4">
+        <SearchCard/>
         <div className="row">
           <div className="col-sm-6 col-lg-4 col-xl-3">
             <ProductCard />
