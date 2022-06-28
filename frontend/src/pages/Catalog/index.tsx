928dd41ef61e4266bcff053c0c7309f2a156b0c4
felipeschirmann@gmail.com <felipeschirmann@gmail.com>
2022-06-28 05:35:21 -0300

    Revert "Create SearchCard"
    This reverts commit 1dc9c79444a8ab2d70a9d83360a2cf91d6a6120b.

--- frontend/src/pages/Catalog/index.tsx
+++ frontend/src/pages/Catalog/index.tsx
@@ -1,14 +1,12 @@
 import './styles.css';
 import Navbar from 'components/Navbar';
 import ProductCard from 'components/ProductCard';
-import SearchCard from 'components/SearchCard'
 
 const Catalog = () => {
   return (
     <div>
       <Navbar />
       <div className="container my-4">
-        <SearchCard/>
         <div className="row">
           <div className="col-sm-6 col-lg-4 col-xl-3">
             <ProductCard />
